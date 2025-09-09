1a74f7d4e785f9c215e0b5138922c4473b7a6cce
Andre Cantarino <cantarino.andre@gmail.com>
2025-09-09 08:31:31 -0300

    feat: added bucket random hash name

--- index.ts
+++ index.ts
@@ -1,35 +1,31 @@
 import * as aws from '@pulumi/aws';
+import { randomBytes } from 'crypto';
 
-const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
-    bucket: 'primeiro-bucket-pos-full-stack-360',
-    tags: {
-        IAC: "true",
-    },
+export function generateRandomHash(size: number = 7): string {
+  const bytesNeeded = Math.ceil(size * 0.5);
+  return randomBytes(bytesNeeded).toString('hex').slice(0, size);
+}
+
+const bucketHashName = generateRandomHash();
+
+const firstBucket = new aws.s3.BucketV2(`primeiro-bucket-${bucketHashName}`, {
+  bucket: `primeiro-bucket-pos-full-stack-360-${bucketHashName}`,
+  tags: {
+    IAC: 'true',
+  },
 });
 
-const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
-    bucket: 'segundo-bucket-pos-full-stack-360',
-    tags: {
-        IAC: "true",
-    },
-});
-
-
-const ecr = new aws.ecr.Repository("primeiro-ecr", {
-    name: 'primeiro-ecr',
-    imageTagMutability: 'IMMUTABLE',
-    tags: {
-        IAC: "true",
-    }
+const ecr = new aws.ecr.Repository(`primeiro-ecr-${bucketHashName}`, {
+  name: `primeiro-ecr-${bucketHashName}`,
+  imageTagMutability: 'IMMUTABLE',
+  tags: {
+    IAC: 'true',
+  }
 });
 
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
-
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;

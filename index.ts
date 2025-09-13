5223dc41c3ed862094a73b93762ee4e85aa2168d
Andre Cantarino <cantarino.andre@gmail.com>
2025-09-13 13:58:14 -0300

    fix: added new bucket

--- index.ts
+++ index.ts
@@ -15,6 +15,13 @@ const firstBucket = new aws.s3.BucketV2(`primeiro-bucket-${bucketHashName}`, {
   },
 });
 
+const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
+  bucket: 'segundo-bucket-pos-full-stack-360',
+  tags: {
+      IAC: "true",
+  },
+});
+
 const ecr = new aws.ecr.Repository(`primeiro-ecr-${bucketHashName}`, {
   name: `primeiro-ecr-${bucketHashName}`,
   imageTagMutability: 'IMMUTABLE',
@@ -26,6 +33,10 @@ const ecr = new aws.ecr.Repository(`primeiro-ecr-${bucketHashName}`, {
 export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
